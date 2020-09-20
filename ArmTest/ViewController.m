b34ff7c6a8d9856fc3c93e46a3d2b2ecdc35aba1
yinfan <yinfan@meituan.com>
2020-09-20 16:58:23 +0800

    Initial Commit

---
+++ ArmTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ArmTest
+//
+//  Created by fanyin on 2020/9/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ ArmTestTests/ArmTestTests.m
@@ -0,0 +1,36 @@
+//
+//  ArmTestTests.m
+//  ArmTestTests
+//
+//  Created by fanyin on 2020/9/20.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ArmTestTests : XCTestCase
+
+@end
+
+@implementation ArmTestTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
